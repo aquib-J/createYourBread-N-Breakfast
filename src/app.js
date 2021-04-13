cf381287e94a8c3271b27fdfd4eebcbfe7deb60c
Jansher Aquib / aquib_J <aquib.jansher@gmail.com>
2021-04-14 03:39:59 +0530

    plumbing -2 : express-app-chain setup , orm initialization and db connection setup

--- src/app.js
+++ src/app.js
@@ -1,6 +1,25 @@
-const {Logger}=require('./utils');
-const path=require('path');
-const config=require('./config');
-const initLoader=require('./loaders');
-const express=require('express');
+const { Logger } = require('./utils');
+const config = require('./config');
+const initLoader = require('./loaders');
+const express = require('express');
 
+Logger.init({ level: config.logs.level });
+
+(async () => {
+  try {
+    const app = express();
+
+    await initLoader({ expressApp: app });
+    app.listen(config.port, (err) => {
+      if (err) {
+        Logger.log('error', '', err);
+        process.exit(1);
+        return;
+      }
+      Logger.log('info', `bread and breakfast on port :${config.port}`,);
+    });
+  }catch(err){
+      console.log('error-------------------------------------------------', err);
+      Logger.log('error','',err);
+  }
+})();
--- src/loaders/sequelize.js
+++ src/loaders/sequelize.js
@@ -1,7 +1,37 @@
-const fs=require('fs');
-const path=require('path');
-const Sequelize=require('sequelize');
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
 
-const config=require('../config');
+const config = require('../config');
 
-const 
+const baseName = path.basename(__filename);
+const models = {};
+
+const modelsPath = `${__dirname}/../models`;
+
+Sequelize.Promise.config({
+  longStackTraces: true,
+});
+
+const sequelize = new Sequelize(config.database.name, config.database.user, config.database.password, {
+  host: config.database.host,
+  dialect: config.database.dialect,
+});
+
+fs.readdirSync(modelsPath)
+  .filter((file) => file.indexOf('.') !== 0 && file !== baseName && file.slice(-3) === '.js')
+  .forEach(file=>{
+      const model=sequelize.import(path.join(modelsPath,file));
+      models[model.name]=model;
+  });
+
+  Object.keys(models).forEach(modeName=>{
+      if(models[modelName].associate){
+          models[modelName].associate(models);
+      }
+  });
+
+  module.exports={
+      sequelize,
+      models,
+  }
