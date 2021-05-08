3254ad88f5551aebc7c04503ba91498e57b38cfa
Jansher Aquib / aquib_J <aquib.jansher@gmail.com>
2021-05-09 00:56:29 +0530

    Booking APIs completed

--- src/services/BookingService.js
+++ src/services/BookingService.js
@@ -2,42 +2,39 @@ const { Op } = require('sequelize');
 const { models } = require('../loaders/sequelize');
 const { Logger, Response, Message } = require('../utils');
 const UtilityService = require('./UtilityService');
-const moment = require('moment');
 
-let dd = UtilityService.dateDiffCalculator;
 class BookingService {
   static async createBooking(params) {
     try {
       let cost = 0;
       let obj = {};
       Logger.log('info', 'checking if the Listing is already booked for the current dates');
-      let booking = await models.booking.findOne({
+      let booking = await models.booking.findAll({
         where: {
           [Op.and]: [
             {
               listingId: params.listingId,
-              [Op.and]: [
-                { checkInDate: { [Op.gte]: params.checkInDate } },
-                { checkOutDate: { [Op.lte]: params.checkOutDate } },
+              [Op.or]: [
+                {
+                  [Op.and]: [
+                    { checkOutDate: { [Op.gte]: params.checkInDate } },
+                    { checkInDate: { [Op.lte]: params.checkOutDate } },
+                  ],
+                },
+                {
+                  [Op.and]: [
+                    { checkOutDate: { [Op.lte]: params.checkInDate } },
+                    { checkInDate: { [Op.gte]: params.checkOutDate } },
+                  ],
+                },
               ],
             },
           ],
         },
       });
 
-      if (booking) {
-        booking.get({ plain: true });
-        // 4 + 2 possible cases of slot timings collision eliminated
+      if (booking.length) throw Response.createError(Message.listingAlreadyBooked);
 
-        if (
-          (dd(booking.checkInDate, params.checkInDate) >= 0 && dd(params.checkOutDate, booking.checkOutDate) >= 0) ||
-          (dd(booking.checkInDate, params.checkInDate) <= 0 && dd(params.checkOutDate, booking.checkOutDate) <= 0) ||
-          (dd(booking.checkInDate, params.checkInDate) <= 0 && dd(params.checkOutDate, booking.checkOutDate) >= 0) ||
-          (dd(booking.checkInDate, params.checkInDate) >= 0 && dd(params.checkOutDate, booking.checkOutDate) <= 0)
-        ) {
-          throw Response.createError(Message.listingAlreadyBooked);
-        }
-      }
       obj.checkInDate = params.checkInDate;
       obj.checkOutDate = params.checkOutDate;
       obj.listingId = params.listingId;
@@ -69,7 +66,17 @@ class BookingService {
         where: {
           userId: params.id,
         },
-        raw: true,
+        include: [
+          {
+            model: models.listing,
+            include: [
+              {
+                model: models.image,
+              },
+            ],
+          },
+        ],
+        order: [['checkOutDate', 'DESC']],
       });
       if (user) return { data: user };
       throw Response.createError(Message.BookingNotFound);
